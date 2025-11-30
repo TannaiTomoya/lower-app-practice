d2053b7032e363d8887734bbb50f2e60c97d9f6d
TannaiTomoya <tomobou5912@gmail.com>
2025-11-30 17:30:36 +0900

    大文字を小文字に変換する機能をつけました

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Livewire\Volt\Volt;
 Route::get('/', function () {
     return view('welcome');
 });
+Volt::route('lower', 'lower');
+Volt::route('/lower/{word}', 'lower');
